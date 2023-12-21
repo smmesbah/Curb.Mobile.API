f443316b6ac356d32ba9b7915e1ef4496a2d2027
smmesbah <mesbah@recruit.so>
2023-12-21 11:43:44 +0600

    Add prisma ORM

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
 
 
 
@@ -13,6 +14,8 @@ dotenv.config();
 
 //rest object
 const app = express();
+const prisma = new PrismaClient()
+
 
 //middleware
 app.use(cors());
@@ -27,8 +30,137 @@ app.use(express.json());
 
 
 //test api
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.post('/bookstores', async (req, res) => {
+    try {
+        const { name, address, link, subscribers } = req.body;
+        const bookstore = await prisma.book_stores.create({
+            data: {
+                name,
+                address,
+                link,
+                subscribers,
+            }
+        });
+
+        res.json(bookstore);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error creating book store' });
+    }
+
+})
+app.get('/bookstores', async (req, res) => {
+
+    try {
+
+        const bookstores = await prisma.book_stores.findMany();
+
+        res.json(bookstores);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({ error: 'Error fetching book stores' });
+
+    }
+
+});
+
+app.get('/bookstores/:id', async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+
+        const bookstore = await prisma.book_stores.findUnique({
+
+            where: { id: parseInt(id) },
+
+        });
+
+        if (!bookstore) {
+
+            return res.status(404).json({ error: 'Book store not found' });
+
+        }
+
+        res.json(bookstore);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({ error: 'Error fetching book store' });
+
+    }
+
+});
+
+app.put('/bookstores/:id', async (req, res) => {
+
+    const { id } = req.params;
+
+    const { name, address, link, subscribers } = req.body;
+
+    try {
+
+        const updatedBookstore = await prisma.book_stores.update({
+
+            where: { id: parseInt(id) },
+
+            data: {
+
+                name,
+
+                address,
+
+                link,
+
+                subscribers,
+
+            },
+
+        });
+
+        res.json(updatedBookstore);
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({ error: 'Error updating book store' });
+
+    }
+
+});
+
+app.delete('/bookstores/:id', async (req, res) => {
+
+    const { id } = req.params;
+
+    try {
+
+        await prisma.book_stores.delete({
+
+            where: { id: parseInt(id) },
+
+        });
+
+        res.json({ message: 'Book store deleted successfully' });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({ error: 'Error deleting book store' });
+
+    }
+
 });
 
 //PORT
